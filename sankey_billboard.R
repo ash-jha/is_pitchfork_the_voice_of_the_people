1f11ece7d3f6e8cbd457753465c1fb06d4fd12f4
ash-jha <ashwinijha28@gmail.com>
2019-10-10 18:32:13 -0700

    updated wordcloud and sankey colors

--- sankey_billboard.R
+++ sankey_billboard.R
@@ -4,9 +4,6 @@ library(htmlwidgets)
 library(htmltools)
 library(magrittr)
 
-# nodes_path <- "/Users/ashjha/Documents/USF/mod_1/msds593_data_viz/Final Project/processed_data/df_nodes.feather"
-# links_path <- "/Users/ashjha/Documents/USF/mod_1/msds593_data_viz/Final Project/processed_data/df_links.feather"
-
 nodes_path <- "data/df_nodes.feather"
 links_path <- "data/df_links.feather"
 
@@ -17,9 +14,13 @@ print(df_nodes)
 print(df_links)
 
 sankey_title_string <- "Pitchfork Scores                                                      Peak Position on Billboard Charts"
-
+# color_JS <- JS("d3.scaleOrdinal([\"#1A9641\", \"#A6D96A\", \"#FFFFBF\", \"#FDAE61\", \"#D7191C\",
+#                \"#1A9641\", \"#A6D96A\", \"#FFFFBF\", \"#FDAE61\", \"#D7191C\"])")
+color_JS <- JS("d3.scaleOrdinal([\"#1A9641\", \"#A6D96A\", \"#FFFFBF\", \"#FDAE61\", \"#1A9641\",
+               \"#A6D96A\", \"#FFFFBF\", \"#FDAE61\", \"#D7191C\", \"#D7191C\"])")
+               
 sankeyNetwork(Links = df_links, Nodes = df_nodes, Source = "score_bucket_index",
               Target = "chart_bucket_index", Value = "count", NodeID = "nodes",
               units = "Albums", fontSize = 20, nodeWidth = 20, 
-              fontFamily = "sans-serif", iterations = 0) %>% 
+              fontFamily = "sans-serif", iterations = 0, colourScale=color_JS) %>% 
   htmlwidgets::prependContent(htmltools::tags$pre(sankey_title_string))
