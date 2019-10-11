5b17e185f8db34db95f50445f6ede0d25a02d36c
Sean Tey <stey@dons.usfca.edu>
2019-10-10 21:41:23 -0700

    fix sankey

--- sankey_billboard.R
+++ sankey_billboard.R
@@ -13,7 +13,8 @@ df_links <- read_feather(links_path)
 print(df_nodes)
 print(df_links)
 
-sankey_title_string <- "Pitchfork Scores                                                      Peak Position on Billboard Charts"
+sankey_title_string <- "Pitchfork                                                                                     Peak Position on 
+ Scores                                                                              Billboard Charts"
 # color_JS <- JS("d3.scaleOrdinal([\"#1A9641\", \"#A6D96A\", \"#FFFFBF\", \"#FDAE61\", \"#D7191C\",
 #                \"#1A9641\", \"#A6D96A\", \"#FFFFBF\", \"#FDAE61\", \"#D7191C\"])")
 color_JS <- JS("d3.scaleOrdinal([\"#1A9641\", \"#A6D96A\", \"#FFFFBF\", \"#FDAE61\", \"#1A9641\",
@@ -21,6 +22,8 @@ color_JS <- JS("d3.scaleOrdinal([\"#1A9641\", \"#A6D96A\", \"#FFFFBF\", \"#FDAE6
                
 sankeyNetwork(Links = df_links, Nodes = df_nodes, Source = "score_bucket_index",
               Target = "chart_bucket_index", Value = "count", NodeID = "nodes",
-              units = "Albums", fontSize = 20, nodeWidth = 20, 
-              fontFamily = "sans-serif", iterations = 0, colourScale=color_JS) %>% 
-  htmlwidgets::prependContent(htmltools::tags$pre(sankey_title_string))
+              units = "Albums", fontSize = 20, nodeWidth = 20,
+              fontFamily = "Arial", iterations = 0, colourScale=color_JS) %>% 
+  htmlwidgets::prependContent(htmltools::tags$pre(sankey_title_string),
+                              htmltools::tags$style("pre{font-family:Arial;font-size:15px; margin:0px;font-weight: bold;}"))
+
